8bdb2112b723ac8d66e656426b9c79a277ac8608
azhar <azhar199865@gmail.com>
2019-07-18 17:27:20 +0530

    updated init for dataloader

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,3 +3,5 @@ from dataset.icdar2015_test_loader import IC15TestLoader
 
 from dataset.ctw1500_loader import CTW1500Loader
 from dataset.ctw1500_test_loader import CTW1500TestLoader
+
+from dataset.icdar2019MLT_loader import IC19Loader 
